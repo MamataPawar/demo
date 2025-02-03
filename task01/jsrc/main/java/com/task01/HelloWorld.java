ee84654af1d6232f0166973654d7ece0f5f485db
Mamata Pawar <mammu89@live.com>
2025-02-03 18:09:28 +0530

    draft

--- task01/jsrc/main/java/com/task01/HelloWorld.java
+++ task01/jsrc/main/java/com/task01/HelloWorld.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(
-    lambdaName = "hello_world_test",
+    lambdaName = "hello_world",
 	roleName = "hello_world-role",
 	isPublishVersion = false,
 	aliasName = "${lambdas_alias_name}",
